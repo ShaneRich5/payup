0a35e0cd29955f58b5cb6d410bc6212b5a7886ab
Shane Richards <shane.richards121@gmail.com>
2025-10-31 09:32:47 -0500

    feat: including uuid column; #2

--- database/migrations/2025_10_31_142620_create_payment_requests_table.php
+++ database/migrations/2025_10_31_142620_create_payment_requests_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Str;
 
 return new class extends Migration
 {
@@ -13,6 +14,8 @@ return new class extends Migration
     {
         Schema::create('payment_requests', function (Blueprint $table) {
             $table->id();
+
+            $table->uuid('uuid')->unique();
 
             $table->foreignId('payment_account_id')->constrained()->cascadeOnDelete();
             $table->foreignId('owner_id')->constrained('users')->cascadeOnDelete();
